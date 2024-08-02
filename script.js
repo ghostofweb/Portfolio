b29916038be90ddeded3714b840bd7079259155f
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-08-02 21:28:00 +0530

    resume update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function toggleMobileMenu(){
-    document.getElementById("menu").classList.toggle("active");
-  }
+var total = 0;
+var a = [4,6,2,12];
+a.forEach(x => {
+   total += x;
+});
+
+console.log(total);
+
+const reducer = (acc,current) => acc + current;
+console.log(a.reduce(reducer));
